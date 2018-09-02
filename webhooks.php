674fa23196e06a4c4f3c21b1734bb433afe165ee
Napakadol <42905702+Napakadol@users.noreply.github.com>
2018-09-02 14:23:57 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'ENyqht/iVslJdNhpfAzYHZY3vn6uOCO6/uFdXYcSvZlZMflwnRAfctoObeisHs4DDN8A/TG4XTanJQWFlth8WE70+3y9W1TJYwpX1gJz7uNWPMeTA/zswfPfFfdo3qgJNgmjL6GjjaBPyb/5WHgqdQdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
